24de81fabae3adbfc6fbab060119f1673d3b6304
Scott Hillier <scotthhillier@gmail.com>
2021-11-23 09:36:11 -0800

    Added tables and data for teachers and assistance requests

--- 3_queries_group-by/5_less_than_average_assignment_time.sql
+++ 3_queries_group-by/5_less_than_average_assignment_time.sql
@@ -1,6 +1,8 @@
-SELECT students.name as student, SUM(assignment_submissions.duration)/COUNT(assignment_submissions.duration) as average_assignment_duration, SUM(assignment.duration)/COUNT(assignment.duration) as average_estimated_duration
-FROM assignment_submissions
-JOIN students ON student_id = students.id
-JOIN assignments ON assignment_id = assignmen.id
-WHERE student.end_date IS NULL
-ORDER BY average_assignment_duration DESC;
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
