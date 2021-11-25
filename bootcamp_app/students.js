24de84232dd8267c12e492caca7c838070779eda
Scott Hillier <scotthhillier@gmail.com>
2021-11-24 17:27:50 -0800

    Protecting from sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,18 +7,21 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-console.log("HELLO");
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE '%${cohortName}%'
+LIMIT ${limit};
+`;
+
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
